f761a4073bb00660b9084f22b69c5df10de36494
nvsajjad <nvsajjad@gmail.com>
2020-09-08 14:51:54 +0530

    card components creation

--- src/app/admin/admin-components/admin-components.module.ts
+++ src/app/admin/admin-components/admin-components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TileComponent } from './tile/tile.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
-  declarations: [TileComponent],
+  declarations: [TileComponent, CardComponent],
   imports: [
     CommonModule
   ],
