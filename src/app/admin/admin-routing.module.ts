c32c2a351324ed9481935d346a2894e9a138ca14
nvsajjad <nvsajjad@gmail.com>
2020-09-16 15:15:49 +0530

    variant updated

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,7 +5,7 @@ import { AdminGuard } from 'app/guards/admin/admin.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
 import { ProductFormComponent } from './product-form/product-form.component';
-import {VariantFormComponent} from './variant-form/variant-form.component'
+import {VariantFormComponent} from './variant-form/variant-form.component';
 
 
 const routes: Routes = [
