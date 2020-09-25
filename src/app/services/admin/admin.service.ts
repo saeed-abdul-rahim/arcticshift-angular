e14b9a7efdc276bb4f874ae989c54efe553de8db
sajjad bin sadique <nvsajjad@gmail.com>
2020-09-25 16:28:29 +0530

    update

--- src/app/services/admin/admin.service.ts
+++ src/app/services/admin/admin.service.ts
@@ -9,14 +9,8 @@ import { CategoryInterface } from '@models/Category';
 import { VoucherInterface } from '@models/Voucher';
 import { SaleDiscountInterface } from '@models/SaleDiscount';
 import { VariantInterface } from '@models/Variant';
-<<<<<<< HEAD
-import { ProductTypeInterface } from '@models/ProductType';
-import { AttributeInterface } from '@models/Attribute';
-import { WarehouseInterface } from '@models/Warehouse';
-=======
 import {  getDataFromDocument } from '@utils/getFirestoreData';
 import { AuthService } from '@services/auth/auth.service';
->>>>>>> 4b55895facfeae1303155c608651b8c25d909d9f
 
 @Injectable()
 export class AdminService {
@@ -34,40 +28,18 @@ export class AdminService {
   private db: AngularFirestoreDocument;
   private dbAnalytics: AngularFirestoreCollection;
 
-<<<<<<< HEAD
-  constructor(private req: RequestService) {
-    const { api } = environment;
-    const { url,
-      product,
-      category,
-      collection,
-      sale,
-      variant,
-      voucher,
-      productType,
-      attribute,
-      warehouse
-    } = api;
-=======
   constructor(private req: RequestService, private afs: AngularFirestore, private auth: AuthService) {
     const { api, db } = environment;
     const { url, product, category, collection, sale, variant, voucher } = api;
     const { version, name, products, analytics } = db;
->>>>>>> 4b55895facfeae1303155c608651b8c25d909d9f
     this.apiProduct = url + product;
     this.apiCategory = url + category;
     this.apiCollection = url + collection;
     this.apiSale = url + sale;
     this.apiVariant = url + variant;
     this.apiVoucher = url + voucher;
-<<<<<<< HEAD
-    this.apiProductType = url + productType;
-    this.apiAttribute = url + attribute;
-    this.apiWarehouse = url + warehouse;
-=======
     this.db = this.afs.collection(version).doc(name);
     this.dbAnalytics = this.db.collection(analytics);
->>>>>>> 4b55895facfeae1303155c608651b8c25d909d9f
   }
 
   async createProduct(data: ProductInterface) {
@@ -172,72 +144,8 @@ export class AdminService {
     }
   }
 
-<<<<<<< HEAD
-  async updateVoucher(data: VoucherInterface) {
-    const { apiVoucher } = this;
-    try {
-      return await this.req.patch(apiVoucher, { data });
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async createProductType(data: ProductTypeInterface) {
-    const { apiProductType } = this;
-    try {
-      return await this.req.post(apiProductType, { data });
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async updateProductType(data: ProductTypeInterface) {
-    const { apiProductType } = this;
-    try {
-      return await this.req.patch(apiProductType, { data });
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async createAttribute(data: AttributeInterface) {
-    const { apiAttribute } = this;
-    try {
-      return await this.req.post(apiAttribute, { data });
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async updateAttribute(data: AttributeInterface) {
-    const { apiAttribute } = this;
-    try {
-      return await this.req.patch(apiAttribute, { data });
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async createWarehouse(data: WarehouseInterface) {
-    const { apiWarehouse } = this;
-    try {
-      return await this.req.post(apiWarehouse, { data });
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  async updateWarehouse(data: WarehouseInterface) {
-    const { apiWarehouse } = this;
-    try {
-      return await this.req.patch(apiWarehouse, { data });
-    } catch (err) {
-      throw err;
-    }
-=======
   getCollectionAnalytics(path: string) {
     return getDataFromDocument(this.dbAnalytics.doc(path));
->>>>>>> 4b55895facfeae1303155c608651b8c25d909d9f
   }
 
 }
