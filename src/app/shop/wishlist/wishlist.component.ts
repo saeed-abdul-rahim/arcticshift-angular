bf2e4bf9617ccdbd2ab619c531dfe020c8a5b3f7
sajjad bin sadique <nvsajjad@gmail.com>
2020-11-19 13:15:57 +0530

    wishlist updates

--- src/app/shop/wishlist/wishlist.component.ts
+++ src/app/shop/wishlist/wishlist.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductInterface } from '@models/Product';
+
 import { UserInterface } from '@models/User';
 import { AuthService } from '@services/auth/auth.service';
 import { Subscription } from 'rxjs';
 import { shopProductRoute } from '@constants/routes';
 import { ShopService } from '@services/shop/shop.service';
+import { setSaleDiscountForProduct } from '@utils/productUtils';
+import { SaleDiscountInterface } from '@models/SaleDiscount';
+import { IMAGE_L } from '@constants/imageSize';
+import { setThumbnails } from '@utils/media';
+import { ValueType } from '@models/Common';
 
 
 
@@ -19,16 +25,17 @@ export class WishlistComponent implements OnInit, OnDestroy {
 
   showSearch = false;
 
+  saleDiscounts: SaleDiscountInterface[];
   user: UserInterface;
   wishlistSubscription: Subscription;
-  products: ProductInterface[] = [];
+  products: ProductInterface & SaleDiscountInterface[] = [];
 
 
   constructor(private auth: AuthService, private router: Router, private shop: ShopService) { }
 
 
   ngOnInit(): void {
-   this.getWishlist();
+    this.getWishlist();
   }
 
   ngOnDestroy(): void {
@@ -37,13 +44,46 @@ export class WishlistComponent implements OnInit, OnDestroy {
     }
   }
   getWishlist() {
-    this.wishlistSubscription = this.auth.getCurrentUserDocument().subscribe(user => this.user = user);
-    if (this.user){
-    const { wishlist } = this.user;
-    this.shop.getProductbyIds(wishlist).subscribe(product => this.products = product);
-
+    this.wishlistSubscription = this.auth.getCurrentUserDocument().subscribe(user => {
+      this.user = user; console.log(user);
+      if (this.user) {
+        const { wishlist } = this.user;
+        console.log(wishlist);
+        this.shop.getProductbyIds(wishlist).subscribe(products => {
+          this.products = products.filter(p => p);
+          console.log(this.products);
+        },
+          error => console.log(error)
+        );
+        if (wishlist.length > 0) {
+          return console.log(wishlist);
+        }
+      }
     }
+    );
   }
+
+  setProducts(products?: ProductInterface[]) {
+      this.products = products.map(product => {
+        if (!product) { return; }
+        const { id, images, price, name } = product;
+        const thumbnails = setThumbnails(images, name, IMAGE_L);
+        let discountValue: number;
+        let discountType: ValueType;
+        if (this.saleDiscounts) {
+          const productDiscount = setSaleDiscountForProduct(this.saleDiscounts, product);
+          discountType = productDiscount.discountType;
+          discountValue = productDiscount.discountValue;
+        }
+        return {
+          id, price, name,
+          images: thumbnails,
+          value: discountValue,
+          valueType: discountType
+        };
+      }).filter(e => e);
+    }
+
 
   closeSearch() {
     this.showSearch = false;
